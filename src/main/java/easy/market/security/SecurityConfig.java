6f188d127392f6e8e8b0bb99b94b9a1a5f209cca
baejeu <zeus20240228@gmail.com>
2025-02-02 20:49:13 +0900

    [2025:02:02] 로그인(인증) 필터 구현, 추가

--- src/main/java/easy/market/security/SecurityConfig.java
+++ src/main/java/easy/market/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package easy.market.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,11 +16,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-
+    private final AuthenticationConfiguration configuration;
+    private final ObjectMapper objectMapper;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)  throws Exception {
+        return configuration.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,6 +40,10 @@ public class SecurityConfig {
         http
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(configuration), objectMapper),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
