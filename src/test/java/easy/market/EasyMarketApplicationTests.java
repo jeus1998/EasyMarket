9170de3e247bb8139b9549839e75e24376277c73
baejeu <zeus20240228@gmail.com>
2025-02-02 09:27:46 +0900

    [2025:02:02] 프로젝트 생성

---
+++ src/test/java/easy/market/EasyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package easy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
