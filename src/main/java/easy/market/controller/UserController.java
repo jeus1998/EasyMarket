fe5ff79cfa9ea9d372a8a4aa59a2d971f683f046
baejeu <zeus20240228@gmail.com>
2025-02-03 16:43:13 +0900

    [2025:02:03] 로그인 폼 구현

--- src/main/java/easy/market/controller/UserController.java
+++ src/main/java/easy/market/controller/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     @GetMapping("/join")
     public String join() {
-        return "join";
+        return "/user/join";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "/user/login";
     }
 }
